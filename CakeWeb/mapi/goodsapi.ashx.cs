e9df2ab0439bf6df534ad9fc4b77053bfedbe039
shaojixing@qq.com <shaojixing@qq.com>
2016-06-13 17:12:10 +0800

    11

--- CakeWeb/mapi/goodsapi.ashx.cs
+++ CakeWeb/mapi/goodsapi.ashx.cs
@@ -18,52 +18,128 @@ namespace CakeWeb.mapi
         {
             context.Response.ContentType = "text/plain";
             // context.Response.Write("Hello World");
+            
+            string action = context.Request["action"].ToString();
+            switch (action)
+            { 
+                case "product":
+                    GetProduct(context);
+                    break;
+                case "detail":
+                    GetDetail(context);
+                    break;
+                case "commont":
+                    GetCommomont(context);
+                    break;
+                case "getlist":
+                    GetListById(context);
+                    break;
+
+
+            }
+          
+
+        }
+
+        private void GetListById(HttpContext context)
+        {
+            string pid = context.Request["pid"].ToString();
+            int wd = Convert.ToInt32(pid);
+            List<int?> id = new List<int?>();
+            id.Add(1);
+            id.Add(2);
+            id.Add(3);
+            if (pid != null)
+            {
+                List<T_Protuct> plist = new List<T_Protuct>();
+
+                ProductBll bll = new ProductBll();
+
+                plist = bll.GetProductListByClassId(id);
+                if (plist != null)
+                {
+                    string productjson = JsonHelper.GetJson(plist);
+                    context.Response.Write(productjson);
+                    context.Response.End();
+                }
+            }
+        }
+
+        private void GetProduct(HttpContext context)
+        {
             string pid = context.Request["pid"].ToString();
             int wd = Convert.ToInt32(pid);
             if (pid != null)
             {
 
                 T_Protuct product = new T_Protuct();
-                List<T_ProductDetail> pdetail = new List<T_ProductDetail>();
-                List<T_ProductComment> pcommon = new List<T_ProductComment>();
+             
                 ProductBll bll = new ProductBll();
                 product = bll.GetProductById(wd);
-                pdetail = bll.GetProductDetailById(wd);
+             
                 if (product != null)
                 {
-                    // JsonHelper jsonhelper = new JsonHelper();
-                    // jsonhelper.
-                   
-                    //Imgurl1 = product.Imgurl1;
-                    //Imgurl2 = product.Imgurl2;
-                    //Imgurl3 = product.Imgurl3;
-                    //producttitle = product.ProductTitle;
-                    //productdec = product.productdec;
-                    //price = product.Price.ToString();
-                    // pcontent = product.ProductContent;
-                    // this.strcontent.InnerHtml = product.ProductContent;
-                    //  pcommon = bll.GetCommentByPId(wd);
-
-
-                }
-                if (pdetail != null)
-                {
                     string productjson = JsonHelper.GetJson(product);
-                        //+ JsonHelper.Obj2Json(pdetail);
-
-                    
                     context.Response.Write(productjson);
                     context.Response.End();
-                //    this.mid.Value = pdetail[0].Price.ToString();
-
-                //    dllist.DataSource = pdetail;
-                //    dllist.DataBind();
                 }
-                //if (pcommon != null)
-                //{
-                //    DataList1.DataSource = pcommon;
-                //    DataList1.DataBind();
-                //}
+
+            }
+        }
+
+        private void GetDetail(HttpContext context)
+        {
+            string pid = context.Request["pid"].ToString();
+            int wd = Convert.ToInt32(pid);
+            if (pid != null)
+            {
+
+               // T_Protuct product = new T_Protuct();
+                List<T_ProductDetail> pdetail = new List<T_ProductDetail>();
+              //  List<T_ProductComment> pcommon = new List<T_ProductComment>();
+                ProductBll bll = new ProductBll();
+               // product = bll.GetProductById(wd);
+                pdetail = bll.GetProductDetailById(wd);
+                if (pdetail != null)
+                {
+                    string productjson = JsonHelper.GetJson(pdetail);
+                    context.Response.Write(productjson);
+                    context.Response.End();
+                }
+
+            }
+        }
+        private void GetCommomont(HttpContext context)
+        {
+            string pid = context.Request["pid"].ToString();
+            int wd = Convert.ToInt32(pid);
+            if (pid != null)
+            {
+
+                // T_Protuct product = new T_Protuct();
+                // List<T_ProductDetail> pdetail = new List<T_ProductDetail>();
+                List<T_ProductComment> pcommon = new List<T_ProductComment>();
+                ProductBll bll = new ProductBll();
+                // product = bll.GetProductById(wd);
+                //pdetail = bll.GetProductDetailById(wd);
+                pcommon = bll.GetCommentByPId(wd);
+                if (pcommon != null)
+                {
+                    string productjson = JsonHelper.GetJson(pcommon);
+                    context.Response.Write(productjson);
+                    context.Response.End();
+                }
+                else
+                {
+                    context.Response.Write("0");
+                    context.Response.End();
+                }
+
+            }
+            else
+            {
+                context.Response.Write("0");
+                context.Response.End();
             }
 
         }
